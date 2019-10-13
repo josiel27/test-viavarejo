317643416fb3fad25696091da3a6bf33463357f4
josielsifa <josielsifa@gmail.com>
2019-10-13 15:22:03 -0300

    Updates

---
+++ precache-manifest.597fed7a335cdbd52f68131f38d4850e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12a087abc44ed2d45af5fabfcbd4d8e",
+    "url": "/test-viavarejo/index.html"
+  },
+  {
+    "revision": "caa2ddaab08b74283aef",
+    "url": "/test-viavarejo/static/css/main.b2e4875c.chunk.css"
+  },
+  {
+    "revision": "542f1ef91466d863d714",
+    "url": "/test-viavarejo/static/js/2.4cab9a4c.chunk.js"
+  },
+  {
+    "revision": "caa2ddaab08b74283aef",
+    "url": "/test-viavarejo/static/js/main.daefa96d.chunk.js"
+  },
+  {
+    "revision": "2b7c69943415c9e3b2bd",
+    "url": "/test-viavarejo/static/js/runtime-main.bdd26b81.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/test-viavarejo/static/media/Lato-Regular.2d36b1a9.ttf"
+  }
+]);
